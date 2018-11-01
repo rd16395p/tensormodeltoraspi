afb4680a15c935da514ddee13529850537839eab
Becca <40492703+rd16395p@users.noreply.github.com>
2018-11-01 14:52:32 -0400

    Update picameraevalver2.py

--- picameraevalver2.py
+++ picameraevalver2.py
@@ -16,10 +16,10 @@ with picamera.PiCamera() as camera:
         camera.resolution = (100, 100)
         camera.framerate = 24
         time.sleep(2)
-        output = np.empty((112 * 128 * 3,), dtype=np.uint8)
-        camera.capture(output, 'rgb')
+        #output = np.empty((112 * 128 * 3,), dtype=np.uint8)
+        camera.capture(output, 'rgb',resize=(100, 100))
         
-output = output.reshape((112, 128, 3))
+#output = output.reshape((112, 128, 3))
 output = output[:100, :100, :]
 output = output.reshape((100, 100, 3))
     #camera.capture(output, 'rgb')
